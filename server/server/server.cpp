eabac57cd55025a7123162e664b69cac4b1af99b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-01-26 16:41:45 +0530

    Initialization of winsock and listner socket created

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "WS2_32.lib") //Winsock Library
 
 using namespace std;
 
 int main() {
+
+	// Initialize winsock
+
+	// Create socket
+
+	// Bind the socket to an ip address and port
+
+	// Tell winsock the socket is for listening
+
+	// Wait for connetction
+
+	// Close listening socket
+
+	// While loop: accept and echo message back to client
+
+	// Close socket
+
+	// Shutdown winsock
 
 	return 0;
 }
