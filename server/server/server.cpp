0c6cc0c09b72021fce4f2bc45ba038c6395a3805
Don Heshanthaka <heshanthaka007@gmail.com>
2022-01-26 16:53:01 +0530

    Binding the listner socket

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -8,10 +8,33 @@ using namespace std;
 int main() {
 
 	// Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wSock = WSAStartup(ver, &wsData);
+	if (wSock != 0) {
+		cerr << "Winsock initialization failed!" << endl;
+		return 0;
+	}
 
 	// Create socket
 
-	// Bind the socket to an ip address and port
+	SOCKET listenerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenerSocket == INVALID_SOCKET) {
+		cerr << "Socket creation failed!" << endl;
+		return 0;
+	}
+
+	// Bind the ip address and port to a socket
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(listenerSocket, (sockaddr*)&hint, sizeof(hint));
+
+
 
 	// Tell winsock the socket is for listening
 
